3ad7381cea070e896597abe7ab2ae22a438eeba1
DudakXen <dudak.xen@yandex.ru>
2024-06-25 16:52:23 +0300

    Исправляет ошибки линетра

--- js/functions.js
+++ js/functions.js
@@ -1,32 +1,34 @@
 function getStringLength (string, number) {
-  return(string.length <= number)
+  return(string.length <= number);
 }
-/* Проверка фукнции getStringLength
-console.log(getStringLength('проверяемая строка', 20))
-console.log(getStringLength('проверяемая строка', 18))
-console.log(getStringLength('проверяемая строка', 10))
-*/
 
-function isPalindrom(string){
-let firstString = string.replaceAll(' ','').toUpperCase();
-let secondString = '';
-for(let i = (firstString.length - 1); i >= 0; i --){
-secondString += firstString[i]
+// Проверка фукнции getStringLength
+getStringLength('проверяемая строка', 20);
+getStringLength('проверяемая строка', 18);
+getStringLength('проверяемая строка', 10);
+
+
+function isPalindrom(string) {
+  const firstString = string.replaceAll(' ','').toUpperCase();
+  let secondString = '';
+  for(let i = (firstString.length - 1); i >= 0; i --){
+    secondString += firstString[i];
+  }
+  return secondString === firstString;
 }
-return secondString === firstString;
-}
-/* Проверка фукнции isPalindrom
-console.log(isPalindrom('топот'));
-console.log(isPalindrom('ДовОд'));
-console.log(isPalindrom('Кекс'));
-console.log(isPalindrom('Лёша на полке клопа нашёл '));
-*/
-function getNumber(param){
+
+// Проверка фукнции isPalindrom
+isPalindrom('топот');
+isPalindrom('ДовОд');
+isPalindrom('Кекс');
+isPalindrom('Лёша на полке клопа нашёл ');
+
+function getNumber(param) {
   let newNumber = '';
-  let firstString = param.toString();
+  const firstString = param.toString();
 
   for(let i = 0; i <= firstString.length; i++){
-    let q = parseInt(firstString[i], 10);
+    const q = parseInt(firstString[i], 10);
 
     if (!Number.isNaN(q)){
       newNumber += q;
@@ -34,14 +36,14 @@ function getNumber(param){
   }
   return (newNumber === '' ? NaN : newNumber);
 }
-/* Проверка фукнции getNumber
-console.log(getNumber('2023 год'));
-console.log(getNumber('ECMAScript 2022'));
-console.log(getNumber('1 кефир, 0.5 батона'));
-console.log(getNumber('агент 007'));
-console.log(getNumber('а я томат'));
-console.log(getNumber(2023));
-console.log(getNumber(-1));
-console.log(getNumber(1.5));
-*/
+// Проверка фукнции getNumber
+getNumber('2023 год');
+getNumber('ECMAScript 2022');
+getNumber('1 кефир, 0.5 батона');
+getNumber('агент 007');
+getNumber('а я томат');
+getNumber(2023);
+getNumber(-1);
+getNumber(1.5);
+
 
