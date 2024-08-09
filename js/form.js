76d7e937b5d53719dd40b0110580dd72071df75e
DudakXen <dudak.xen@yandex.ru>
2024-08-09 16:51:14 +0300

    тестирование

--- js/form.js
+++ js/form.js
@@ -15,7 +15,7 @@ const SubmitButtonText = {
 
 const body = document.querySelector('body');
 const form = document.querySelector('.img-upload__form');
-const overlay = form.querySelector('.img-upload__overlay');
+const overlay = document.querySelector('.img-upload__overlay');
 const cancelButton = form.querySelector('.img-upload__cancel');
 const fileField = form.querySelector('.img-upload__input');
 const hashtagField = form.querySelector('.text__hashtags');
@@ -25,18 +25,18 @@ const photoPreview = form.querySelector('.img-upload__preview img');
 const effectsPreviews = form.querySelectorAll('.effects__preview');
 
 const showModal = () => {//показать модалку
-  overlay.classList.remove('hidden');
   body.classList.add('modal-open');
+  overlay.classList.remove('hidden');
   document.addEventListener('keydown', onDocumentKeydown);
   cancelButton.addEventListener('click', onCancelButtonClick);
 };
 
 const hideModal = () => {//скрыть модалку
+  overlay.classList.add('hidden');
   form.reset();//сбросить значения формы
   pristine.reset();//сбросить значения валидатора
   resetScale();//сбросить значения масштабирования
   resetEffect();//сбросить значения фильтров
-  overlay.classList.add('hidden');
   body.classList.remove('modal-open');
   document.removeEventListener('keydown', onDocumentKeydown);
   cancelButton.removeEventListener('click', onCancelButtonClick);
